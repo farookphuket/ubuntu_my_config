c85de2ccfa929a3491dedefe383f6aa46b41eb88
farook <farookphuket@gmail.com>
2021-07-07 07:21:30 +0700

    update from Ubuntu 7 July 2021 06:39 a.m. added ocenaudio

---
+++ I3WM_SETUP/install_ocenaudio.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+
+# install ocenaudio to replace audacity as audacity will be no longer 
+# play nice to the free software 
+# https://www.ocenaudio.com/start_download/ocenaudio_debian9_64.deb
+
+pushd ~/Downloads/ 
+
+# call firefox to open the download link 
+firefox -- https://www.ocenaudio.com/start_download/ocenaudio_debian9_64.deb
+
+sudo dbkg -i ocenaudio_debian9_64_3.10.9.deb 
+
+
+
+echo "========================= Dear $USER ==================================="
+echo " I don't know if this going to install as I can't install this using "
+echo " the command line the link to download will be :"
+echo " https://www.ocenaudio.com/start_download/ocenaudio_debian9_64.deb " 
+echo " you can type the command sudo dpkg -i follow by the file name"
+echo "========================================================================"
+
+sleep 5s
+
+popd 
